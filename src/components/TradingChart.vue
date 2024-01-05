589bb01157e6d1760ba80dabaa469bb6aa33a865
alex1431999 <alex@baserow.io>
2024-01-05 22:29:45 +0100

    feat(chart): draw a simple blue line

--- src/components/TradingChart.vue
+++ src/components/TradingChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <canvas></canvas>
+  <canvas width="1200" height="1200" ref="canvas"></canvas>
 </template>
 
 <script lang="ts">
@@ -7,5 +7,28 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'TradingChart',
+  computed: {
+    canvas(): HTMLCanvasElement {
+      return this.$refs.canvas as HTMLCanvasElement;
+    },
+    ctx(): CanvasRenderingContext2D {
+      const context = this.canvas.getContext('2d');
+
+      if (!context) {
+        throw Error('The canvas was not rendered');
+      }
+
+      return context;
+    },
+    lineColor() {
+      return '#3498db';
+    },
+  },
+  mounted() {
+    this.ctx.moveTo(0, 0);
+    this.ctx.lineTo(200, 200);
+    this.ctx.strokeStyle = this.lineColor;
+    this.ctx.stroke();
+  },
 });
 </script>
